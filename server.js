f943cd328ed42a6af9e03a70fde7c7056fcbcf0b
Borborem <uli77e7@yahoo.com.br>
2026-04-17 20:53:50 -0300

    last atempt

--- server.js
+++ server.js
@@ -31,6 +31,7 @@ io.on('connection', socket => {
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
-});
+server.listen(PORT, '0.0.0.0');
+// server.listen(PORT, () => {
+//   console.log(`Servidor rodando na porta ${PORT}`);
+// });
